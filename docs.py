7a6e3cd7811ea2c0b73d984943665226687543e9
ruchith696 <34888753+ruchith696@users.noreply.github.com>
2020-04-27 22:19:09 +0530

    Delete docs.py

--- docs.py
+++
@@ -1,5 +0,0 @@
-import docx
-doc = docx.Document()
-doc.add_paragraph('testing')
-doc.save('test.docx')
-#doc.add_picture('image.jpg', width= docx.shared.Inches(4), height- docx.shared.Inches(3)) to add image to docx
